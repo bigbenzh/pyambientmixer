d9179b5648a32f95927ca9e2f1404dbaaf1d5792
bigbenzh <85002534+bigbenzh@users.noreply.github.com>
2021-08-04 18:08:10 -0400

    change to async

--- ambient_downloader.py
+++ ambient_downloader.py
@@ -4,7 +4,7 @@ Usage:
   ambient_downloader.py <url>
  
 Options:
-  <url>				 URL of the ambient mix.
+  <url>                 URL of the ambient mix.
   -h --help          Show this help message.
  
 """
@@ -15,58 +15,67 @@ import re, os
 
 import requests
 import untangle
+import aiohttp
+import aiofiles
+import asyncio
 
 template_url = "https://xml.ambient-mixer.com/audio-template?player=html5&id_template="
 re_js_reg = re.compile(r"AmbientMixer.setup\(([0-9]+)\);")
 
 def makedirs():
-	if not os.path.exists("sounds"):
-		os.makedirs("sounds")
-	if not os.path.exists("presets"):
-		os.makedirs("presets")
+    if not os.path.exists("sounds"):
+        os.makedirs("sounds")
+    if not os.path.exists("presets"):
+        os.makedirs("presets")
 
-def download_file(url, save = False, filename = None):
-	if(len(url.strip()) == 0):
-		return
-	response = requests.get(url)
-	if(save):
-		if filename is None:
-			filename = url.split('/')[-1]
-		with open(filename, "wb") as file:
-			file.write(response.content)
-		print("Saved {} as {}.".format(url, filename))
-	else:
-		return response.text
+async def download_file(url, save = False, filename = None):
+    if(len(url.strip()) == 0):
+        return
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url)
+        content = await response.read()
+    if(save):
+        if filename is None:
+            filename = url.split('/')[-1]
+        async with aiofiles.open(filename,"wb") as f:
+            await f.write(content)
+        print("Saved {} as {}.".format(url, filename))
+    else:
+        return await response.read()
 
-def get_correct_file(url, filename = None):
-	if(filename is None):
-		filename = url.split("/")[-1]
-	print(url)
-	if(not url.startswith(template_url)):
-			page = download_file(url)
-			val = re_js_reg.findall(str(page))[0]
-			url = template_url + val
-	fname = os.path.join("presets", "{}.xml".format(filename))
-	download_file(url, True, fname)
-	return fname
+async def get_correct_file(url, filename = None):
+    if(filename is None):
+        filename = url.split("/")[-1]
+    print(url)
+    if(not url.startswith(template_url)):
+            page = await download_file(url)
+            val = re_js_reg.findall(str(page))[0]
+            url = template_url + val
+    fname = os.path.join("presets", "{}.xml".format(filename))
+    await download_file(url, True, fname)
+    return fname
 
-def download_sounds(xml_file):
-	obj = untangle.parse(xml_file)
-	for chan_num in range(1,9):
-		channel = getattr(obj.audio_template, "channel{}".format(chan_num))
-		new_filename = channel.id_audio.cdata
-		url = channel.url_audio.cdata
-		url_ogg = channel.url_audio.cdata.replace(".mp3",".ogg")
-		ext = url.split('.')[-1]
-		filename = os.path.join("sounds", new_filename + "." + ext)
-		filename_ogg = os.path.join("sounds", new_filename + ".ogg")
-		if not(os.path.exists(filename) or os.path.exists(filename_ogg)):
-			download_file(url, True, filename)
-			download_file(url_ogg, True, filename_ogg)
+async def download_sounds(xml_file):
+    obj = untangle.parse(xml_file)
+    tasks = []
+    for chan_num in range(1,9):
+        channel = getattr(obj.audio_template, "channel{}".format(chan_num))
+        new_filename = channel.id_audio.cdata
+        url = channel.url_audio.cdata
+        url_ogg = channel.url_audio.cdata.replace(".mp3",".ogg")
+        ext = url.split('.')[-1]
+        filename = os.path.join("sounds", new_filename + "." + ext)
+        filename_ogg = os.path.join("sounds", new_filename + ".ogg")
+        if not(os.path.exists(filename) or os.path.exists(filename_ogg)):
+            tasks += [download_file(url, True, filename),download_file(url_ogg, True, filename_ogg)]
+    await asyncio.gather(*tasks)
 
+async def helper(arg):
+    xml_file = await get_correct_file(arg)
+    await download_sounds(xml_file)
 
 from docopt import docopt
 if __name__ == "__main__":
-	arguments = docopt(__doc__, version = '0.1ß')
-	makedirs()
-	download_sounds(get_correct_file(arguments.get('<url>')))
+    arguments = docopt(__doc__, version = '0.1ß')
+    makedirs()
+    asyncio.get_event_loop().run_until_complete(helper(arguments.get('<url>')))
